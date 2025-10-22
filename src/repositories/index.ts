36d834481c3bc3f0f2e2eb06cdb899d36f2d47a2
LrVanAnhDuc <levananhduc1804@gmail.com>
2025-10-22 22:51:21 +0700

    refactor(server): remove unused todo and password reset features

--- src/repositories/index.ts
+++
@@ -1,7 +0,0 @@
-import TodoRepo from "./todo.repo";
-import UserResetPasswordTokenRepo from "./passwordResetToken.repo";
-
-const todoRepo = new TodoRepo();
-const userResetPasswordTokenRepo = new UserResetPasswordTokenRepo();
-
-export { todoRepo, userResetPasswordTokenRepo };
