98f893a135c347b66a9d70d27478ab2e5cb41c19
LrVanAnhDuc <levananhduc1804@gmail.com>
2026-03-04 23:44:32 +0700

    feat(user): TASK-005 add express.static to serve uploads/ directory

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { requestLogger } from "@/middlewares/request-logger";
 import { i18nMiddleware } from "./i18n";
 import { setupSwagger } from "@/config/swagger.setup";
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(requestLogger);
 app.use(i18nMiddleware);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 setupSwagger(app);
 
