3643e3b13d5decb6a976f42c61147884678c8abc
LrVanAnhDuc <levananhduc1804@gmail.com>
2026-03-28 14:37:52 +0700

    feat: add Middleware interface to core/common

--- src/core/common/index.ts
+++ src/core/common/index.ts
@@ -1,5 +1,9 @@
-import type { Request } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 export interface CanActivate {
   canActivate(req: Request): boolean | void | Promise<boolean | void>;
+}
+
+export interface Middleware {
+  use(req: Request, res: Response, next: NextFunction): void | Promise<void>;
 }
