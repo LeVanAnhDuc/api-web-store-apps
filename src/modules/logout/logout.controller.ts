693a8ab4cb27010106f64362f1230fc0ad3274fb
LrVanAnhDuc <levananhduc1804@gmail.com>
2026-03-29 13:49:12 +0700

    refactor

--- src/modules/logout/logout.controller.ts
+++ src/modules/logout/logout.controller.ts
@@ -1,25 +1,21 @@
 // libs
-import type { Request, Response } from "express";
+import type { Response } from "express";
 // types
+import type { LogoutRequest } from "@/types/modules/logout";
 import type { LogoutService } from "./logout.service";
 // config
 import { NoContentSuccess } from "@/config/responses/success";
-import ENV from "@/config/env";
+import { REFRESH_TOKEN_COOKIE_OPTIONS } from "@/config/cookie";
 // others
 import { REFRESH_TOKEN } from "@/constants/modules/token";
 
 export class LogoutController {
   constructor(private readonly service: LogoutService) {}
 
-  logout = async (req: Request, res: Response): Promise<void> => {
-    await this.service.logout(req);
+  logout = async (req: LogoutRequest, res: Response): Promise<void> => {
+    await this.service.logout(req.user!.userId);
 
-    res.clearCookie(REFRESH_TOKEN, {
-      httpOnly: true,
-      secure: ENV.NODE_ENV === "production",
-      sameSite: "lax",
-      path: "/"
-    });
+    res.clearCookie(REFRESH_TOKEN, REFRESH_TOKEN_COOKIE_OPTIONS);
 
     new NoContentSuccess().send(req, res);
   };
--- src/modules/logout/logout.service.ts
+++ src/modules/logout/logout.service.ts
@@ -1,13 +1,8 @@
-// libs
-import type { Request } from "express";
 // others
 import { Logger } from "@/utils/logger";
 
 export class LogoutService {
-  async logout(req: Request): Promise<void> {
-    const userId = req.user?.userId;
-
-    Logger.info("Logout initiated", { userId });
+  async logout(userId: string): Promise<void> {
     Logger.info("Logout successful", { userId });
   }
 }
