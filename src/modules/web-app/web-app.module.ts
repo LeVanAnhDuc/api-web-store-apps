b42d81ac8144167c3f66b7568338a82ec4d93b45
LrVanAnhDuc <levananhduc1804@gmail.com>
2026-06-07 11:05:23 +0700

    feat(web-app): wire web app module into loader

---
+++ src/modules/web-app/web-app.module.ts
@@ -0,0 +1,19 @@
+// others
+import {
+  MongoWebAppRepository,
+  MongoWebAppCategoryRepository
+} from "./repositories";
+import { WebAppService } from "./web-app.service";
+import { WebAppController } from "./web-app.controller";
+import { createAdminWebAppRoutes } from "./web-app.routes";
+
+export const createWebAppModule = () => {
+  const webAppRepo = new MongoWebAppRepository();
+  const categoryRepo = new MongoWebAppCategoryRepository();
+  const service = new WebAppService(webAppRepo, categoryRepo);
+  const controller = new WebAppController(service);
+
+  return {
+    webAppAdminRouter: createAdminWebAppRoutes(controller)
+  };
+};
