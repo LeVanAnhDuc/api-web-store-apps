3de290f13c843882849b1ba2bae9538c0a1e02ef
LrVanAnhDuc <levananhduc1804@gmail.com>
2026-06-20 17:02:44 +0700

    refactor(email): introduce Mailer interface, depend on abstraction

--- src/services/email/email.dispatcher.ts
+++ src/services/email/email.dispatcher.ts
@@ -1,12 +1,15 @@
 // types
-import type { SendEmailService } from "./email.service";
 import type { QueueService } from "@/services/queue/queue.service";
 import type { EmailJobData } from "@/types/services/queue";
-import type { EmailType, SendEmailOptions } from "@/types/services/email";
+import type {
+  EmailType,
+  Mailer,
+  SendEmailOptions
+} from "@/types/services/email";
 
 export class EmailDispatcher {
   constructor(
-    private readonly emailService: SendEmailService,
+    private readonly emailService: Mailer,
     private readonly emailQueue: QueueService<EmailJobData> | null
   ) {}
 
